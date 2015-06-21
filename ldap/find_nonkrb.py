d9c20e3aeb1c0ecd69e8ce58cd55ed3f80b58270
Matt Hazinski <matt@matthazinski.com>
2015-06-21 18:09:57 -0400

    Add LDAP scripts

---
+++ ldap/find_nonkrb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+###############################################################################
+# find_nonkrb.py - find users that have not migrated to kerberos
+#
+# author: mutantmonkey <mutantmonkey@mutantmonkey.in>
+###############################################################################
+
+import ldap
+import lib
+
+l = lib.connect()
+
+results = l.search_s('ou=People,dc=vtluug,dc=org', ldap.SCOPE_SUBTREE)
+for result in results:
+    props = dict(result[1])
+    if not 'userPassword' in props:
+        continue
+    if not props['userPassword'][0].startswith('{SASL}'):
+        print(result[0])
