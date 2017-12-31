a6e9993cf739e8a0efc1a29d6ba39838adb536ba
paulwalko <paulsw.pw@gmail.com>
2017-12-31 04:23:01 -0500

    typo

---
+++ networking/setup_router.sh
@@ -0,0 +1,13 @@
+MACHINES=('
+    joey.vtluug.org
+    crashandburn.vtluug.org
+    sczi.vtluug.org
+    acidburn.vtluug.org
+    zerocool.vtluug.org
+    mirror.vtluug.org
+')
+
+for machine in $MACHINES
+do
+    echo $machine
+done
