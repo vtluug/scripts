7fd02e04c93c452fd6323cd8bde4488af7e8f249
Paul Walko <paulsw.pw@gmail.com>
2018-04-05 17:42:11 -0400

    typo

--- router/lan/vtluug_iptables.sh
+++ router/lan/vtluug_iptables.sh
@@ -3,4 +3,4 @@
 # VTLUUG iptables things
 
 # Nat
- iptables --table nat --append POSTROUTING --source 10.98.0.0/16 --out-interface enp2s0 --jump SNAT --to 128.173.88.191
+iptables --table nat --append POSTROUTING --source 10.98.0.0/16 --out-interface enp2s0 --jump SNAT --to 128.173.88.191
