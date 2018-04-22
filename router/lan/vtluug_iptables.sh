58c51611c1deb5387175e5110f3bfca61f075c6a
Paul Walko <paulsw.pw@gmail.com>
2018-04-22 15:12:33 -0400

    update dnsmasq config

--- router/lan/vtluug_iptables.sh
+++ router/lan/vtluug_iptables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
 # VTLUUG iptables things
 
+echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
 # Nat
-iptables --table nat --append POSTROUTING --source 10.98.0.0/16 --out-interface enp2s0 --jump SNAT --to 128.173.88.191
+#iptables --table nat --append POSTROUTING --source 10.98.0.0/16 --out-interface enp2s0 --jump SNAT --to 128.173.88.191
+# Temp for nested nat
+iptables --table nat --append POSTROUTING --source 10.98.0.0/16 --out-interface enp2s0 --jump SNAT --to 128.173.89.246
