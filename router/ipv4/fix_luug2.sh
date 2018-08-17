dd8688d4114591c1a34177d4d48320788dc22f50
Paul Walko <paulsw.pw@gmail.com>
2018-08-17 17:13:00 -0400

    Remove old ipv4 & ipv6 configuration

--- router/ipv4/fix_luug2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while true; do
-	echo 1 > /proc/sys/net/ipv4/ip_nonlocal_bind
-	arping -q -I eth0 -U -c 2 -s 128.173.89.246 128.173.91.254
-	sleep 120
-done
-
-#echo 0 > /proc/sys/net/ipv4/ip_nonlocal_bind
--- router/ipv6/fix_mirror_routes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ip -6 route del 2001:468:c80:6103:20e:a6ff:fe8b:ea42 dev eth1 metric 0
-ip -6 route add 2001:468:c80:6103:20e:a6ff:fe8b:ea42 dev eth1 metric 0
