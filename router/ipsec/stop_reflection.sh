91fc432c101c1f3f9b5aa33cf9a4a6b48a4f383e
Paul Walko <paulsw.pw@gmail.com>
2018-10-17 11:58:16 -0400

    add some comments (mainly for myself)

--- router/ipsec/stop_reflection.sh
+++ router/ipsec/stop_reflection.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #set -x
 
-protocols_to_block="ftp-data ftp telnet dns kerberos pop2 pop3 sunrpc  ms-rpc netbios-ns netbios-dgm netbios-ssn snmp snmptrap imap3 ldap microsoft-ds kpasswd printer ipp ldaps kerberos-adm kerberos-iv samba-swat pop3s nfs  mysql rdp  krb524 postgres"
-protocols_to_block="20       21  23     52  88       109  110  111     135    137        138         139         161  162      220   389  445          464     515     631 636   749          750         901        995   2049 3306  3389 4444   5432"
+#                   ftp-data ftp telnet dns kerberos pop2 pop3 sunrpc  ms-rpc netbios-ns netbios-dgm netbios-ssn snmp snmptrap imap3 ldap microsoft-ds kpasswd printer ipp ldaps kerberls-adm kerberls-iv samba-seat pop3s nfs  mysql rdp  krb524 postgres
+protocols_to_block='20       21  23     52  88       109  110  111     135    137        138         139         161  162      220   389  445          464     515     631 636   749          750         901        995   2049 3306  3389 4444   5432'
 
-
+# Private IP ranges
 valid_ranges='
     128.173.0.0/16
     198.82.0.0/16
@@ -21,6 +21,7 @@ valid_ranges='
 
 if [[ ${1} = "start" ]] ; then
 
+    # Create chain to *only* match all ips in valud_ranges {{{
     /sbin/iptables --new-chain REFLECTION_source
     /sbin/iptables --new-chain REFLECTION_destination
 
@@ -33,7 +34,9 @@ if [[ ${1} = "start" ]] ; then
         /sbin/iptables --append REFLECTION_destination --destination ${range} --jump ACCEPT
     done
     /sbin/iptables --append REFLECTION_destination                            --jump DROP
+    # }}}
 
+    # Send all 'protocols_to_block' to above chain {{{
     for p in ${protocols_to_block} ; do
         /sbin/iptables --insert INPUT   1 --protocol TCP --destination-port ${p} --jump REFLECTION_source
         /sbin/iptables --insert OUTPUT  1 --protocol TCP --destination-port ${p} --jump REFLECTION_source
@@ -42,9 +45,11 @@ if [[ ${1} = "start" ]] ; then
         /sbin/iptables --insert OUTPUT  1 --protocol UDP --destination-port ${p} --jump REFLECTION_source
         /sbin/iptables --insert FORWARD 1 --protocol UDP --destination-port ${p} --jump REFLECTION_source
     done
+    # }}}
 
 elif [[ ${1} = "stop" ]] ; then
 
+    # Delete rules to send all protocols to chain {{{
     for p in ${protocols_to_block} ; do
         /sbin/iptables --delete INPUT   --protocol TCP --destination-port ${p} --jump REFLECTION_source
         /sbin/iptables --delete OUTPUT  --protocol TCP --destination-port ${p} --jump REFLECTION_source
@@ -53,7 +58,9 @@ elif [[ ${1} = "stop" ]] ; then
         /sbin/iptables --delete OUTPUT  --protocol UDP --destination-port ${p} --jump REFLECTION_source
         /sbin/iptables --delete FORWARD --protocol UDP --destination-port ${p} --jump REFLECTION_source
     done
+    # }}}
 
+    # Delete chain for allowed ranges {{{
     for range in ${valid_ranges} ; do
         /sbin/iptables --delete REFLECTION_destination --destination ${range} --jump ACCEPT
     done
@@ -66,6 +73,7 @@ elif [[ ${1} = "stop" ]] ; then
 
     /sbin/iptables --delete-chain REFLECTION_source
     /sbin/iptables --delete-chain REFLECTION_destination
+    # }}}
 else
     echo "Usage: ${0} [start|stop]"
 fi
