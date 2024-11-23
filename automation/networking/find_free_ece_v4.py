8c67193d8f7f0e36a005a65075d8784d030eb8e6
rsk <rsk@vtluug.org>
2024-11-23 04:28:16 -0500

    the nis ip alloc api returns odd json

--- automation/networking/find_free_ece_v4.py
+++ automation/networking/find_free_ece_v4.py
@@ -1,4 +1,4 @@
-import requests, os, ipaddress
+import requests, os, ipaddress, json
 
 ece = "128.173.88.1/22"
 addrs = ipaddress.IPv4Network(ece, False)
@@ -10,8 +10,8 @@ for i in addrs:
 	if ret == 0: continue
 	r = requests.get("https://ipinfo.io/"+str(i)+"/json").json()
 	if r.get("hostname") is not None: continue
-	r = requests.get("https://orca-public.caas.nis.vt.edu/ipr/v1/public/ip/"+str(i)+"/contacts").json()
-	if r[0].get("adminGroup") is not "ECE": continue
+	r = requests.get("https://orca-public.caas.nis.vt.edu/ipr/v1/public/ip/"+str(i)+"/contacts").content.decode("ascii")
+	if not "ECE" in r: continue
 	good.append(str(i))
 	print(good)
 
