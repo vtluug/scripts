a547a10c9aa8d57166820ce20aa60b65af58f319
cynic <kurisufag1@gmail.com>
2024-11-23 04:17:07 -0500

    don't pull from the nis/cns ip space

--- automation/networking/find_free_ece_v4.py
+++ automation/networking/find_free_ece_v4.py
@@ -10,6 +10,8 @@ for i in addrs:
 	if ret == 0: continue
 	r = requests.get("https://ipinfo.io/"+str(i)+"/json").json()
 	if r.get("hostname") is not None: continue
+	r = requests.get("https://orca-public.caas.nis.vt.edu/ipr/v1/public/ip/"+str(i)+"/contacts").json()
+	if r[0].get("adminGroup") is not "ECE": continue
 	good.append(str(i))
 	print(good)
 
