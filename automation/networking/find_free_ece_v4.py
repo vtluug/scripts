f34bc197a5bec7a685f80aecb05ec75ab9532492
cynic <kurisufag1@gmail.com>
2025-01-24 16:24:05 -0500

    working ip search script

--- automation/networking/find_free_ece_v4.py
+++ automation/networking/find_free_ece_v4.py
@@ -6,13 +6,23 @@ addrs = ipaddress.IPv4Network(ece, False)
 good = []
 
 for i in addrs:
-	ret = os.system("ping -c 1 -W .5 "+str(i))
-	if ret == 0: continue
+	print(i, end="")
+	ret = os.system("ping -c 1 -W .5 "+str(i)+" 1> /dev/null")
+	if ret == 0:
+		print(" BAD PING")
+		continue
 	r = requests.get("https://ipinfo.io/"+str(i)+"/json").json()
-	if r.get("hostname") is not None: continue
+	if r.get("hostname") is not None:
+		print(" BAD RDNS "+r.get("hostname"))
+		continue
 	r = requests.get("https://orca-public.caas.nis.vt.edu/ipr/v1/public/ip/"+str(i)+"/contacts").content.decode("ascii")
-	if not "ECE" in r: continue
+	if not "adminGroup" in r:
+		print(" BAD NIS RESPONSE")
+		continue
+	if not "ECE" in r:
+		print(" NOT ECE "+json.loads(r)[0]["adminGroup"])
+		continue
 	good.append(str(i))
-	print(good)
+	print(" GOOD")
 
 print(good)
